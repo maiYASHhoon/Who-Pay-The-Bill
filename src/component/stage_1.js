816b920850dc5469350e1c37ef6d1c917831f791
maiYASHhoon <yash280801@gmail.com>
2023-06-27 13:18:45 +0530

    form

--- src/component/stage_1.js
+++ src/component/stage_1.js
@@ -1,5 +1,26 @@
+import React, {useState, useContext, useRef} from "react";
+import {Button, Form, Alert} from "react-bootstrap";
 const Stage1 = () => {
-    return <>stage 1</>;
+    const textInput = useRef();
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const value = textInput.current.value;
+
+        console.log(value);
+    };
+
+    return (
+        <>
+            <Form onSubmit={handleSubmit} className="mt-4">
+                <Form.Group>
+                    <Form.Control type="text" placeholder="Add Player Name" name="player" ref={textInput} />
+                </Form.Group>
+                <button className="miami" variant="primary" type="submit">
+                    Add Player
+                </button>
+            </Form>
+        </>
+    );
 };
 
 export default Stage1;
